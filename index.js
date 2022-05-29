e351cc5f474387f13bcce2cf9097632ce886891d
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-05-29 16:33:29 -0500

    changes to index.js

--- index.js
+++ index.js
@@ -50,10 +50,52 @@ const promptManager = () => {
                     return false;
                 }
             }
+        },
+        {
+           type: 'input',
+           name: 'officeNumber',
+           message: 'What is your office number? (Required)',
+           validate: officeNumber => {
+               if (officeNumber) {
+                   return true;
+               } else {
+                   console.log('Please enter your office number!');
+                   return false;
+               }
+           }
         }
 
-    ]);
+    ])
+    .then(answer => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        promptMenu();
+    })
 };
+
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Please select which option you would like to continue with:',
+            choices: ['add Engineer', 'add Intern', 'finish building my team']
+        }
+    ])
+    .then(userChoice => {
+        switch (userChoice.menu) {
+            case 'add Engineer':
+                promptEngineer();
+                break;
+            case 'add Intern':
+                promptIntern();
+                break;
+            default:
+                buildTeam();
+        }
+    });
+}
 
 function init() {
     promptManager()
